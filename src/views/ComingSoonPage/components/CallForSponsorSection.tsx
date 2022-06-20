91082383f5a3b21f8bd99fd0a04871ef09a83d98
codemilli <hckrmoon@gmail.com>
2022-06-21 08:30:05 +0900

    Call for Sponsor, AdditionalNote 추가

--- src/views/ComingSoonPage/components/CallForSponsorSection.tsx
+++ src/views/ComingSoonPage/components/CallForSponsorSection.tsx
@@ -1,13 +1,13 @@
 import {FC, useRef} from "react";
 import styled from '@emotion/styled';
 import ApplyButton from "~/views/components/ApplyButton";
-import Portal from "~/views/components/Portal";
 import useFadeInOutAnimation from "~/views/ComingSoonPage/hooks/useFadeInOutAnimation";
 import SectionContainer from "~/views/ComingSoonPage/components/SectionContainer";
 import Center from "~/views/ComingSoonPage/components/Center";
 import {tablet} from "~/views/ComingSoonPage/styles/media-query";
 import {SPONSOR_FORM} from "~/views/ComingSoonPage/data/meta";
 import CircleIcon from "~/views/ComingSoonPage/components/CircleIcon";
+import Portal from "~/views/components/Portal";
 
 interface Props {
   state: SectionState;
@@ -21,18 +21,24 @@ const CallForSponsorSection: FC<Props> = ({ state }) => {
       <Center visible={state.visible}>
         <TextWrap style={{ opacity, transform: `scale(${scale})`, transition }}>
           <h2>
-            개발 문화를 선도하는<br/>
+            FE 개발 생태와 함께하는<br/>
             <span>후원사<CircleIcon visible={state.visible}/></span>가 되어주세요.
           </h2>
           <p>
             FECONF 후원사가 되어 개발 문화를 만들고,<br/>
-            기업 홍와 채용 활동을 계획하세요.
+            기업 홍보와 채용 활동을 계획하세요.
           </p>
           <ApplyButton href={SPONSOR_FORM} style={{ pointerEvents: state.visible ? 'all' : 'none' }}>
             후원 문의하기
           </ApplyButton>
         </TextWrap>
       </Center>
+      <Portal area="content">
+        <AdditionalNote style={{ opacity, transition }}>
+          <img src="/images/icons/bolt.png" alt="Notes"/>
+          <p>금액 및 자세한 프로그램은 7월 중순<br/> 입력해주신 연락처를 통해 별도 안내 예정입니다.</p>
+        </AdditionalNote>
+      </Portal>
     </SectionContainer>
   );
 };
@@ -68,6 +74,28 @@ const TextWrap = styled.div`
       font-size: 16px;
     }
   `}
+`;
+
+const AdditionalNote = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 80px;
+  text-align: center;
+
+  img {
+    display: inline-block;
+    width: 32px;
+    height: 32px;
+  }
+
+  p {
+    margin-top: 12px;
+    font-size: 17px;
+    font-weight: 600;
+    line-height: 1.6;
+    color: white;
+  }
 `;
 
 export default CallForSponsorSection;
