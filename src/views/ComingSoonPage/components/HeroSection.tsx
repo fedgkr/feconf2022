427347af1ac12d8cfbded64be59c3b178378829a
codemilli <hckrmoon@gmail.com>
2022-06-22 09:54:50 +0900

    ScrollDown 아이콘 추가

--- src/views/ComingSoonPage/components/HeroSection.tsx
+++ src/views/ComingSoonPage/components/HeroSection.tsx
@@ -5,6 +5,7 @@ import Center from "~/views/ComingSoonPage/components/Center";
 import useFadeInOutAnimation from "~/views/ComingSoonPage/hooks/useFadeInOutAnimation";
 import SectionContainer from "~/views/ComingSoonPage/components/SectionContainer";
 import {mobile, tablet} from "~/views/ComingSoonPage/styles/media-query";
+import Portal from "~/views/components/Portal";
 
 interface Props {
   state: SectionState;
@@ -21,6 +22,14 @@ const HeroSection: FC<Props> = ({ state }) => {
           <h4>국내 최대 프론트엔드 개발 컨퍼런스,<br/> FECONF 2022가 찾아옵니다.</h4>
         </TextWrap>
       </Center>
+      <Portal area="content">
+        <ScrollDown style={{ opacity, transition }}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+            <path d="M4 8L16 16L28 8" stroke="#B0BECF" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M4 18L16 26L28 18" stroke="#B0BECF" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+        </ScrollDown>
+      </Portal>
     </SectionContainer>
   );
 };
@@ -71,6 +80,42 @@ const TextWrap = styled.div`
     ${mobile`
       font-size: 16px;
     `}
+  }
+`;
+
+const ScrollDown = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 64px;
+  pointer-events: none;
+  svg {
+    display: block;
+    width: 32px;
+    height: 32px;
+    margin: 0 auto;
+    animation: arrow 2.4s ease-in-out infinite;
+  }
+  ${tablet`
+    bottom: 32px;
+    svg {
+      width: 24px;
+      height: 24px;
+    }
+  `}
+  @keyframes arrow {
+    0% {
+      opacity: 1;
+      transform: translateY(0) scale(1);
+    }
+    60% {
+      opacity: 0.6;
+      transform: translateY(12px) scale(0.9);
+    }
+    0% {
+      opacity: 1;
+      transform: translateY(0) scale(1);
+    }
   }
 `;
 
