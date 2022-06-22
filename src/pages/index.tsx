af6a814e6741f08a4c0feca1e86a4bdb2efcd328
codemilli <hckrmoon@gmail.com>
2022-06-22 14:43:18 +0900

    Meta 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import Head from "next/head";
+import ComingSoonMeta from "~/views/ComingSoonPage/components/ComingSoonMeta";
 
 export default function App() {
   return (
-    <Head>
-      <meta httpEquiv="refresh" content="0; url=/trailer"/>
-    </Head>
+    <>
+      <Head>
+        <meta httpEquiv="refresh" content="0; url=/trailer"/>
+      </Head>
+      <ComingSoonMeta/>
+    </>
   );
 }
