7ae726ed8a4bc7068e3d1bb7564ce2f19cc5ee6f
codemilli <hckrmoon@gmail.com>
2022-09-01 10:33:06 +0900

    ga 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,21 @@ const Document = () => {
           type="text/css"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
         />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-68676515-5"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-68676515-5');
+        `,
+          }}
+        ></script>
       </Head>
       <body>
         <Main />
