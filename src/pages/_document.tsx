044119d9f885cde49e1f8a42bdc873eb704c96b4
codemilli <hckrmoon@gmail.com>
2022-09-01 11:48:00 +0900

    ga 변경

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 const Document = () => {
   return (
@@ -10,19 +9,20 @@ const Document = () => {
           type="text/css"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
         />
-        <Script
-          strategy="lazyOnload"
-          src="https://www.googletagmanager.com/gtag/js?id=UA-68676515-5"
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=UA-68676515-5`}
         />
         <script
           dangerouslySetInnerHTML={{
             __html: `
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'UA-68676515-5');
-        `,
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-68676515-5', {
+              page_path: window.location.pathname,
+            });
+          `,
           }}
         />
       </Head>
